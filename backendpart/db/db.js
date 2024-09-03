9291e3c804f2f4090741532c028affb727e3c042
areeba-imran2 <areebaimran.edu@gmail.com>
2024-09-03 19:48:35 +0500

    Update db.js

--- backendpart/db/db.js
+++ backendpart/db/db.js
@@ -6,7 +6,7 @@ const client = new Client({
 });
 
 client.connect()
-    .then(() => console.log('Connected to the database'));
+    .then(() => console.log('Connected to the database'))
+    .catch(err => console.error('Connection error', err.stack));
 
 module.exports = client;
-
